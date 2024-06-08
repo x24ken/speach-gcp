ec14cf8abcc7c56e51b755f2eedc0ff1af140259
x24ken <x24ken@gmail.com>
2024-06-08 21:57:11 +0900

    Update home page layout with flexbox styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen bg-slate-500">
       <Button>Click me</Button>
     </div>
   );
